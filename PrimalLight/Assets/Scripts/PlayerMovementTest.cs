54adcc18e187a97eb3f85a95735b0baac9273065
Tiago Jose Sousa Magalhães <up201607931@fe.up.pt>
2019-04-03 11:31:18 +0100

    added player movement test scenario

---
+++ PrimalLight/Assets/Scripts/PlayerMovementTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementTest : MonoBehaviour
+{
+    public float Speed = 5.0f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = (new Vector3(moveHorizontal, 0.0f, moveVertical))*Speed;
+        movement.y = rb.velocity.y;
+
+        rb.velocity = movement;
+    }
+}
