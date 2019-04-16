e137aa3424ac7136ad3a2194d6fa9bfa18ef160f
Quaresma1997 <up201503005@fe.up.pt>
2019-04-16 12:29:16 +0100

    Mirror Puzzle Halfway. Missing mirrors and target

---
+++ PrimalLight/Assets/Scripts/Puzzles/Laser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer lr;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.startWidth = 0.03f;
+        lr.endWidth = 0.03f;
+        lr.SetPosition(0, transform.position);
+        lr.startColor = Color.red;
+        lr.endColor = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+    public LineRenderer getLineRenderer(){
+        return lr;
+    }
+}
