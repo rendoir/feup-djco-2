45b19a86fe4cb3823a2e9ce90606972c096f3e36
Daniel Marques <up201503822@fe.up.pt>
2019-04-20 12:44:02 +0100

    Player can now shoot thunderbolts that don't hit objects!

--- PrimalLight/Assets/Scripts/Player/Attack.cs
+++ PrimalLight/Assets/Scripts/Player/Attack.cs
@@ -4,6 +4,7 @@ using DigitalRuby.LightningBolt;
 public class Attack : MonoBehaviour
 {
     public LightningBoltScript lightning;
+    public LayerMask ignoreMask;
     public float maxDistance = 20f;
 
     void Start()
@@ -18,19 +19,41 @@ public class Attack : MonoBehaviour
             //Cast ray from camera to the mouse position
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            bool miss = false;
 
             //If it hits an object, stick to it
-            if (Physics.Raycast(ray, out hit, maxDistance)) {
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ignoreMask)) {
+                float pw = (hit.point - transform.position).magnitude;
+                if(pw > maxDistance)
+                    miss = true;
+
                 lightning.EndPosition = hit.point;
                 lightning.gameObject.SetActive(true);
-            } else {
+            } else miss = true;
+            
+            if(miss) {
                 //Shoot the lightning at max distance
-                //Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20f));
-                //Vector3 startPosition = lightning.StartObject.transform.position + lightning.StartPosition;
-                //Vector3 maxDistancePosition = startPosition + (worldPosition - startPosition).normalized * maxDistance;
-                //lightning.EndPosition = maxDistancePosition;
-                //lightning.gameObject.SetActive(true);
-                lightning.gameObject.SetActive(false);
+                
+                //Solve pw² = cp² + cw² + 2*cp*cw*cos(c)
+                //Where cos(c) = u_v . CP_v / cp
+                //And u_v is the normalized direction vector cw
+
+                Vector3 cp_vector = (transform.position - Camera.main.transform.position);
+                Vector3 cw_vector = ray.direction;
+                Vector3 u_vector = cw_vector.normalized;
+
+                float pw = maxDistance;
+                float cp = cp_vector.magnitude;
+
+                float a = 1; 
+                float b = -2*cp*Vector3.Dot(u_vector, cp_vector)/cp;
+                float c = -Mathf.Pow(pw,2) + Mathf.Pow(cp,2);
+                float cw = Utils.QuadraticFormula(a, b, c);
+
+                Vector3 w = Camera.main.transform.position + cw * u_vector;
+
+                lightning.EndPosition = w;
+                lightning.gameObject.SetActive(true);
             }
         } else {
             lightning.gameObject.SetActive(false);
--- PrimalLight/Assets/Scripts/Utils/Utils.cs
+++ PrimalLight/Assets/Scripts/Utils/Utils.cs
@@ -4,4 +4,13 @@ static class Utils {
     public static bool MaskContainsLayer(LayerMask mask, LayerMask layer) {
         return ( (1 << layer) & mask) != 0;
     } 
+
+    public static float QuadraticFormula(float a,float b,float c) {
+        float sqrt = Mathf.Sqrt(Mathf.Pow(b,2) - 4*a*c);
+        float plus = (-b + (sqrt)) / (2*a);
+        float minus = (-b - (sqrt)) / (2*a);
+        
+        return plus >= 0 ? plus : minus >= 0 ? minus : 0;
+    }
+
 }
