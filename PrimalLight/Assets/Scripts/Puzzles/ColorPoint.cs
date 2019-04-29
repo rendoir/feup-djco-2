46ad558284866e42b4d4ad193be207979b2ca37f
up201505472 <up201505472@fe.up.pt>
2019-04-29 21:10:59 +0100

    Working on light color puzzle. When the player interacts with the ball he can press R,G and B to increase each color component.

---
+++ PrimalLight/Assets/Scripts/Puzzles/ColorPoint.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPoint : MonoBehaviour, InteractionObserver
+{
+    public InteractionTrigger interactionTrigger;
+
+    public bool isPlayerInteracting;
+    private bool playerCancelled;
+
+    private GameObject laserClone;
+
+    private LineRenderer laserLR;
+    public Color altColor = Color.black;
+    public Renderer rend;
+
+    //I do not know why you need this?
+    void Example()
+    {
+        altColor.g = 0f;
+        altColor.r = 0f;
+        altColor.b = 0f;
+        altColor.a = 0f;
+    }
+
+
+    void Start()
+    {
+        interactionTrigger.SetObserver(this);
+        isPlayerInteracting = false;
+        playerCancelled = false;
+
+        //Call Example to set all color values to zero.
+        Example();
+        //Get the renderer of the object so we can access the color
+        rend = GetComponent<Renderer>();
+        //Set the initial color (0f,0f,0f,0f)
+        rend.material.color = altColor;
+    }
+
+    void FixedUpdate()
+    {
+        if (playerCancelled)
+        {
+            // Debug.Log(laserClone.gameObject);
+            // //When player cancels the puzze, reset the rotation 
+            // Destroy(laserClone.gameObject);
+            playerCancelled = false;
+            return;
+        }
+
+        if (!isPlayerInteracting)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            //Alter the color          
+            altColor.g += 0.1f;
+            //Assign the changed color to the material.
+            rend.material.color = altColor;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //Alter the color           
+            altColor.r += 0.1f;
+            //Assign the changed color to the material. 
+            rend.material.color = altColor;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            //Alter the color            
+            altColor.b += 0.1f;
+            //Assign the changed color to the material. 
+            rend.material.color = altColor;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //Alter the color          
+            altColor.a += 0.1f;
+            //Assign the changed color to the material. 
+            rend.material.color = altColor;
+        }
+    }
+    
+
+    public void OnPlayerInteract()
+    {
+        playerCancelled = isPlayerInteracting;
+        isPlayerInteracting = !isPlayerInteracting;
+        GameManager.CaptureInput(isPlayerInteracting);
+        if (playerCancelled)
+        {
+            return;
+        }
+
+    }
+}
