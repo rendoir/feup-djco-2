87b69298e192ccf761ae4c1f2c1b398f105b63f4
Daniel Marques <up201503822@fe.up.pt>
2019-05-08 16:50:58 +0100

    Changed enemy to spider. Added death animation to enemy

--- PrimalLight/Assets/Scripts/Enemy.cs
+++ PrimalLight/Assets/Scripts/Enemy.cs
@@ -9,40 +9,62 @@ public class Enemy : MonoBehaviour
 
     [Header("Effect")]
     public Renderer meshRenderer;
+    public AnimationControl animationControl;
     private Material material;
 
     private bool isHit;
+    private bool isDead;
 
     void Start()
     {
         health = initialHealth;
         isHit = false;
+        isDead = false;
         material = meshRenderer.material;
     }
 
     void FixedUpdate()
     {
-        if(isHit) {
-            float emission = Mathf.PingPong(Time.time * 2f, 0.2f) + 0.2f;
-            Color baseColor = Color.red;
-            Color finalColor = baseColor * emission;
-            finalColor.a = 1;
-            material.SetColor ("_EmissionColor", finalColor);
-        } else {
-            material.SetColor ("_EmissionColor", Color.black);
-        }
+        UpdateHitEffect();
 
         isHit = false;
     }
 
-    public void OnHit() {
+    public void OnHit()
+    {
         isHit = true;
         health -= Time.deltaTime * healthLoss;
-        
+
         //Enemy died
-        if(health < 0) {
+        if (health < 0)
+        {
+            if (!isDead)
+            {
+                OnDeath();
+            }
+
             health = 0;
-            gameObject.SetActive(false);
         }
-    } 
+    }
+
+    void UpdateHitEffect()
+    {
+        if (!isDead && isHit) {
+            float emission = Mathf.PingPong(Time.time * 2f, 0.2f) + 0.2f;
+            Color baseColor = Color.red;
+            Color finalColor = baseColor * emission;
+            finalColor.a = 1;
+            material.SetColor("_EmissionColor", finalColor);
+
+        }
+        else {
+            material.SetColor("_EmissionColor", Color.black);
+        }
+    }
+
+    void OnDeath()
+    {
+        isDead = true;
+        animationControl.SetAnimation("isDead" + Random.Range(1, 4));
+    }
 }
