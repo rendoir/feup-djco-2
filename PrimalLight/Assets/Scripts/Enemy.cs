befc13f63d3676e17cf92213a14580cb5b02c434
Daniel Marques <up201503822@fe.up.pt>
2019-05-08 15:39:44 +0100

    Added highlight effect on enemy hit

--- PrimalLight/Assets/Scripts/Enemy.cs
+++ PrimalLight/Assets/Scripts/Enemy.cs
@@ -7,17 +7,36 @@ public class Enemy : MonoBehaviour
     public float initialHealth = 20f;
     public float healthLoss = 5f;
 
+    [Header("Effect")]
+    public Renderer meshRenderer;
+    private Material material;
+
+    private bool isHit;
+
     void Start()
     {
         health = initialHealth;
+        isHit = false;
+        material = meshRenderer.material;
     }
 
     void FixedUpdate()
     {
-        //Debug.Log(health);
+        if(isHit) {
+            float emission = Mathf.PingPong(Time.time * 2f, 0.2f) + 0.2f;
+            Color baseColor = Color.red;
+            Color finalColor = baseColor * emission;
+            finalColor.a = 1;
+            material.SetColor ("_EmissionColor", finalColor);
+        } else {
+            material.SetColor ("_EmissionColor", Color.black);
+        }
+
+        isHit = false;
     }
 
     public void OnHit() {
+        isHit = true;
         health -= Time.deltaTime * healthLoss;
         
         //Enemy died
