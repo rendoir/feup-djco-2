4470a805cdbb77159ab78fb952b7488401b36c40
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-05-09 23:48:46 +0100

    lock functionality working

--- PrimalLight/Assets/Scripts/Puzzles/Light Lock/CanisterController.cs
+++ PrimalLight/Assets/Scripts/Puzzles/Light Lock/CanisterController.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class CanisterController : MonoBehaviour
 {
-    public Color CanisterColor;
+    [Header("Game Object References")]
     public GameObject Canister;
     public GameObject MixingStation;
+
+    [Header("Canister Properties")]
+    public Color CanisterColor;
+    public int TotalCanisterFluid = 100;
+    private int CurrentCanisterFluid;
+
+    private MixingStationController mxController;
 
     // Start is called before the first frame update
     void Start()
     {
+        this.setColor(CanisterColor);
+        this.mxController = this.MixingStation.GetComponent<MixingStationController>();
+
+        this.CurrentCanisterFluid = this.TotalCanisterFluid;
+    }
+
+    private void setColor(Color color)
+    {
         Renderer renderer = Canister.GetComponent<Renderer>();
         renderer.material.shader = Shader.Find("_Color");
-        renderer.material.SetColor("_Color", CanisterColor);
+        renderer.material.SetColor("_Color", color);
 
         //Find the Specular shader and change its Color to red
         renderer.material.shader = Shader.Find("Specular");
-        renderer.material.SetColor("_SpecColor", CanisterColor);
+        renderer.material.SetColor("_SpecColor", color);
     }
 }
