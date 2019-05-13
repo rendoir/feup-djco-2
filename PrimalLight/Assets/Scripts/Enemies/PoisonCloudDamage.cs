43fc4ae500ede9b0307198a98ba900af99777252
Daniel Marques <up201503822@fe.up.pt>
2019-05-13 22:18:16 +0100

    Poison cloud now has a duration

--- PrimalLight/Assets/Scripts/Enemies/PoisonCloudDamage.cs
+++ PrimalLight/Assets/Scripts/Enemies/PoisonCloudDamage.cs
@@ -4,7 +4,13 @@ public class PoisonCloudDamage : MonoBehaviour
 {
     public LayerMask playerMask;
     public float healthLoss = 1f;
+    public ParticleSystem cloud;
     Health playerHealth = null;
+
+    void FixedUpdate() {
+        if(!cloud.IsAlive(true))
+            Destroy(gameObject);
+    }
 
     void OnTriggerStay(Collider other) {
         if(Utils.MaskContainsLayer(playerMask, other.gameObject.layer)) {
