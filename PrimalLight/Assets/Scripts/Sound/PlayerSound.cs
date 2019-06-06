e185f32776da845a166c4b555967d20a4da8a3be
Daniel Marques <up201503822@fe.up.pt>
2019-06-06 19:48:18 +0100

    Adds sound on jump landing

--- PrimalLight/Assets/Scripts/Sound/PlayerSound.cs
+++ PrimalLight/Assets/Scripts/Sound/PlayerSound.cs
@@ -4,36 +4,44 @@ using UnityEngine.Audio;
 public class PlayerSound : MonoBehaviour {
     public Animator animator;
     public AudioSource audioSource;
+    public AudioClip jumpClip;
     public AudioClip[] footstepClips;
-    public GameObject leftFoot;
-	public GameObject rightFoot;
 
-	private float currentFrameFootstepLeft;
-	private float currentFrameFootstepRight;
 	private float lastFrameFootstepLeft = 0;
 	private float lastFrameFootstepRight = 0;
+    private bool lastFrameJumping = false;
 
 
 	void Update () 
     {
-		currentFrameFootstepLeft = animator.GetFloat("FootstepLeft");
-		if (currentFrameFootstepLeft > 0 && lastFrameFootstepLeft < 0) {
-			Step();
-        }
-		lastFrameFootstepLeft = animator.GetFloat("FootstepLeft");
+        //Footstep left feet
+		float currentFrameFootstepLeft = animator.GetFloat("FootstepLeft");
+		if (currentFrameFootstepLeft > 0 && lastFrameFootstepLeft < 0)
+			StepSound();
+		lastFrameFootstepLeft = currentFrameFootstepLeft;
 
+        //Footstep right feet
+		float currentFrameFootstepRight = animator.GetFloat("FootstepRight");
+        if (currentFrameFootstepRight < 0 && lastFrameFootstepRight > 0)
+            StepSound();																				
+		lastFrameFootstepRight = currentFrameFootstepRight;
 
-		currentFrameFootstepRight = animator.GetFloat("FootstepRight");
-        if (currentFrameFootstepRight < 0 && lastFrameFootstepRight > 0) {
-            Step();
-        }																					
-		lastFrameFootstepRight = animator.GetFloat("FootstepRight");
+        //Jump
+        bool currentFrameJumping = animator.GetBool("isJumping");
+        if(!currentFrameJumping && lastFrameJumping)
+            JumpSound();
+        lastFrameJumping = currentFrameJumping;
 	}
 
-    public void Step()
+    public void StepSound()
     {
         AudioClip clip = GetRandomClip(footstepClips);
         audioSource.PlayOneShot(clip);
+    }
+
+    public void JumpSound()
+    {
+        audioSource.PlayOneShot(jumpClip);
     }
 
     AudioClip GetRandomClip(AudioClip[] clips) {
