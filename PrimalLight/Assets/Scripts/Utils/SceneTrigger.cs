6eb8e6fe264bfc75219464f3e46cefa4b5fdffb4
Daniel Marques <up201503822@fe.up.pt>
2019-06-11 22:42:37 +0100

    SceneTrigger now allows for initial position/rotation

--- PrimalLight/Assets/Scripts/Utils/SceneTrigger.cs
+++ PrimalLight/Assets/Scripts/Utils/SceneTrigger.cs
@@ -5,13 +5,34 @@ public class SceneTrigger : MonoBehaviour
 {
     public LayerMask playerLayer;
     public int sceneIndex;
-    //public Vector3 initialScenePosition;
-    //public Vector3 initialSceneRotation;
+    public bool overrideTransform = false;
+    public Vector3 initialScenePosition;
+    public Vector3 initialSceneRotation;
+
+    private bool loadingScene = false;
 
     void OnTriggerEnter(Collider other)
     {
         if(Utils.MaskContainsLayer(playerLayer, other.gameObject.layer)) {
             SceneManager.LoadSceneAsync(sceneIndex);
+            loadingScene = true;
         }
     }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+	void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if(loadingScene) {
+            loadingScene = false;
+
+            if(overrideTransform) {
+                GameManager.GetPlayer().transform.position = initialScenePosition;
+                GameManager.GetPlayer().transform.rotation = Quaternion.Euler(initialSceneRotation);
+            }
+        }
+    } 
 }
