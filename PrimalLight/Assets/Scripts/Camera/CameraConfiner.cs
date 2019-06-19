b05bb67282167653dd1f8de8ed532185217eabfc
Kubix20 <dako1oblivion@hotmail.com>
2019-06-19 01:24:15 +0100

    Fixed camera when falling in endless pit

---
+++ PrimalLight/Assets/Scripts/Camera/CameraConfiner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraConfiner : MonoBehaviour
+{
+
+	public CinemachineConfiner cmConfiner;
+	public GameObject confiner;
+
+	public void SetConfiner(){
+		if(confiner == null)
+			cmConfiner.m_BoundingVolume = null;
+		else
+			cmConfiner.m_BoundingVolume = confiner.GetComponent<Collider>();
+	}
+
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player") {
+            SetConfiner();
+        }
+    }
+}
